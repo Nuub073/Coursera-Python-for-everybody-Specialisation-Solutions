8a645ae069a9daf3adada4ae55aa8d5e2e134f9e
Nuub073 <60575125+Nuub073@users.noreply.github.com>
2020-12-30 18:06:28 +0530

    Update Calling_a_json_api.py

--- Course_3/Calling_a_json_api.py
+++ Course_3/Calling_a_json_api.py
@@ -1,3 +1,16 @@
+#In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geojson.py. The program will prompt for a location, contact a web service and retrieve JSON for the web service and parse that data, and retrieve the first place_id from the JSON. A place ID is a textual identifier that uniquely identifies a place as within Google Maps.
+#API End Points
+#To complete this assignment, you should use this API endpoint that has a static subset of the Google Data:
+
+#http://py4e-data.dr-chuck.net/json?
+
+#This API uses the same parameter (address) as the Google API. This API also has no rate limit so you can test as often as you like. If you visit the URL with no parameters, you get "No address..." response.
+#To call the API, you need to include a key= parameter and provide the address that you are requesting as the address= parameter that is properly URL encoded using the urllib.parse.urlencode() function as shown in http://www.py4e.com/code3/geojson.py
+
+
+
+
+
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
